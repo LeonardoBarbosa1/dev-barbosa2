136b36c5bf66d5cbf931259c0864c4f2399ba853
Leonardo Barbosa <leonardobarbosadossantos44@gmail.com>
2025-01-28 12:43:45 -0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "/dev-barbosa2"
+  publicPath: process.env.NODE_ENV === 'production' ? '/dev-barbosa2' : '/'
 })
